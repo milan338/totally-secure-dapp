709592d4ea119b7d06f7e37833b9a09b724232e4
Milan <milan.338@outlook.com>
2022-06-05 15:06:12 +0930

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import { MantineProvider } from '@mantine/core';
+import type { AppProps } from 'next/app';
+
+function MyApp(props: AppProps) {
+    const { Component, pageProps } = props;
+    return (
+        <MantineProvider
+            withGlobalStyles
+            withNormalizeCSS
+            theme={{
+                colorScheme: 'dark',
+            }}
+        >
+            <Component {...pageProps} />
+        </MantineProvider>
+    );
+}
+
+export default MyApp;
