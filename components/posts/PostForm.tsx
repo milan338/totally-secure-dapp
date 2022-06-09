f701203cf2f7f2b6df2a15b9786e88441e245d77
Milan <milan.338@outlook.com>
2022-06-10 00:18:33 +0930

    Add post form loading indicator

--- components/posts/PostForm.tsx
+++ components/posts/PostForm.tsx
@@ -1,40 +1,78 @@
+import abi from '../../abi.json';
 import { useState, useRef } from 'react';
-import { Stack, TextInput, Textarea, Group, Button } from '@mantine/core';
+import { Modal, Stack, TextInput, Textarea, Group, Button, Loader } from '@mantine/core';
+import { Contract } from 'ethers';
+import { useUser } from 'components/context/UserContext';
 import type { Dispatch, SetStateAction } from 'react';
 
 interface PostFormProps {
+    modalOpen: boolean;
     setModalOpen: Dispatch<SetStateAction<boolean>>;
+    editingIndex?: number;
 }
 
 export default function PostForm(props: PostFormProps) {
-    const { setModalOpen } = props;
+    const { modalOpen, setModalOpen, editingIndex } = props;
+    const { user } = useUser();
     const [titleErr, setTitleErr] = useState(false);
     const [contentErr, setContentErr] = useState(false);
+    const [loading, setLoading] = useState(false); // TODO get this able to rerender while stuck inside the async
     const titleRef = useRef<HTMLInputElement>(null);
     const contentRef = useRef<HTMLTextAreaElement>(null);
-    const onSubmit = () => {
-        if (!titleRef.current || !contentRef.current) return;
+    const onSubmit = async () => {
+        if (!titleRef.current || !contentRef.current || !user.contractAddress || !user.provider)
+            return;
         const err = { title: false, content: false };
         if (!titleRef.current.value) err.title = true;
         if (!contentRef.current.value) err.content = true;
         setTitleErr(err.title);
         setContentErr(err.content);
         if (err.title || err.content) return;
-        // setModalOpen(false);
-        // TODO show loading on the button
+        const signer = user.provider.getSigner();
+        const contract = new Contract(user.contractAddress, abi, signer);
+        setLoading(true);
+        try {
+            if (editingIndex !== undefined)
+                await contract.editPost(
+                    editingIndex,
+                    titleRef.current.value,
+                    contentRef.current.value
+                );
+            await contract.addPost(titleRef.current.value, contentRef.current.value);
+            setModalOpen(false);
+        } catch {}
+        setLoading(false);
     };
     return (
-        <Stack>
-            <TextInput ref={titleRef} label="Title" placeholder="Status title" error={titleErr} />
-            <Textarea
-                ref={contentRef}
-                label="Content"
-                placeholder="Status content"
-                error={contentErr}
-            />
-            <Group position="right">
-                <Button onClick={onSubmit}>Submit</Button>
-            </Group>
-        </Stack>
+        <Modal
+            opened={modalOpen}
+            onClose={() => {
+                setModalOpen(false);
+                setLoading(false);
+            }}
+            title="New Post"
+            centered
+        >
+            <Stack>
+                <TextInput ref={titleRef} label="Title" placeholder="Post title" error={titleErr} />
+                <Textarea
+                    ref={contentRef}
+                    label="Content"
+                    placeholder="Post content"
+                    error={contentErr}
+                />
+                <Group position="right">
+                    <Button onClick={() => onSubmit()}>
+                        {loading ? (
+                            <Loader variant="dots" color="white" />
+                        ) : editingIndex === undefined ? (
+                            'Submit post'
+                        ) : (
+                            'Submit edit'
+                        )}
+                    </Button>
+                </Group>
+            </Stack>
+        </Modal>
     );
 }
