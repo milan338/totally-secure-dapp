ea63a2d8e1686241017901e7a19ce2e55f51d49a
Milan <milan.338@outlook.com>
2022-06-10 14:44:03 +0930

    Update batch deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv';
-import { HardhatUserConfig, task } from 'hardhat/config';
+import { HardhatUserConfig, task, types } from 'hardhat/config';
+import { deploy } from './scripts/deploy';
 import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-waffle';
 import '@typechain/hardhat';
@@ -8,12 +9,9 @@ import 'solidity-coverage';
 
 dotenv.config();
 
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners();
-    for (const account of accounts) {
-        console.log(account.address);
-    }
-});
+task('deploy', 'Deploy a number of instances of the contract')
+    .addOptionalParam('instances', 'Number of contract instances to deploy', 1, types.int)
+    .setAction(deploy);
 
 const config: HardhatUserConfig = {
     solidity: '0.4.24',
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,30 +2,27 @@ import serviceAccount from '../serviceAccountKey.json';
 import admin from 'firebase-admin';
 import { FieldValue } from 'firebase-admin/firestore';
 import { nanoid } from 'nanoid';
-import { ethers } from 'hardhat';
+import type { HardhatRuntimeEnvironment } from 'hardhat/types';
 
-const N_DEPLOYMENTS = 5;
-
-async function main() {
+export async function deploy(taskArgs: { instances: number }, hre: HardhatRuntimeEnvironment) {
+    const { instances } = taskArgs;
+    const { ethers } = hre;
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount as any),
     });
     const db = admin.firestore();
     const contractsData = db.collection('contracts').doc('data');
     const TotallySecureDapp = await ethers.getContractFactory('TotallySecureDapp');
-    for (let i = 0; i < N_DEPLOYMENTS; i++) {
+    for (let i = 0; i < instances; i++) {
         const id = nanoid();
+        console.log(`Deploying number ${i} with id ${id}`);
         const totallySecureDapp = await TotallySecureDapp.deploy();
         await totallySecureDapp.deployed();
         await totallySecureDapp.initialize(id);
-        contractsData.update({
+        console.log(`Deployed number ${i} with id ${id} at addr ${totallySecureDapp.address}`);
+        await contractsData.update({
             contracts: FieldValue.arrayUnion(totallySecureDapp.address),
         });
-        console.log(`Deployed number ${i} at addr ${totallySecureDapp.address}`);
     }
+    console.log('Finished');
 }
-
-main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-});
